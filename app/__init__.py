6fe3c31bb044c3812f64e63291624abe7129ad73
jblizz11 <omairekid@yahoo.com>
2016-02-27 01:56:41 +0000

    heroku database link changed

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,13 @@ import urlparse
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is a super secure key"
 app.config['OPENID_PROVIDERS'] = COMMON_PROVIDERS
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+#urlparse.uses_netloc.append("postgres")
+#url = urlparse.urlparse(os.environ["DATABASE_URL"])
 # remember to change to heroku's databas
 #db_conn= 'postgresql+psycopg2://lab5:lab5@localhost/lab5' 
-app.config['SQLALCHEMY_DATABASE_URI'] = url #os.environ['DATABASE_URI']#db_conn
+#app.config['SQLALCHEMY_DATABASE_URI'] = url #os.environ['DATABASE_URI']#db_conn
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://yzicurmnvkqqhp:Kpl-6hLvDaRcPCelsrxdmydSgi@ec2-23-21-219-209.compute-1.amazonaws.com:5432/de7vj5i2j9deb1"
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://ojzmhgzqgzxjmk:lhE-r3F0cCZOcaoaseIPSFaE9Q@ec2-54-83-198-159.compute-1.amazonaws.com:5432/db0p2461btcvuh'
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
